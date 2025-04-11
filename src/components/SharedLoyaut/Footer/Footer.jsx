9a2667b26391f006127982b992dead0a4b0ce646
Serhii Tyron <mestn1y@hotmail.com>
2025-04-11 18:39:23 +0300

    add footer styles

--- src/components/SharedLoyaut/Footer/Footer.jsx
+++ src/components/SharedLoyaut/Footer/Footer.jsx
@@ -1,10 +1,8 @@
 import Logo from "../../Logo/Logo";
-import NavigationLinks from "../Header/NavigationLinks/NavigationLinks";
 import { NavLink } from "react-router";
 import css from "./Footer.module.css";
 import SocialMediaIcons from "./SocialMediaIcons/SocialMediaIcons";
 import FooterBottom from "./FooterBottom/FooterBottom";
-import FooterLinks from "./FooterLinks/FooterLinks";
 export default function Footer() {
   const navLinks = [
     { to: "/home", label: "Home" },
@@ -13,30 +11,39 @@ export default function Footer() {
   ];
   return (
     <footer className={css.footer}>
-      <Logo />
-      <p className={css.text}>
-        Get the medicine to help you feel better, get back to your active life,
-        and enjoy every moment.
-      </p>
-      <ul className={css.navigationList}>
-        {navLinks.map(({ to, label }) => (
-          <li className={css.listItem} key={to}>
-            <NavLink
-              to={to}
-              className={({ isActive }) =>
-                isActive ? `${css.link} ${css.activeLink}` : css.link
-              }
-            >
-              {label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-      <SocialMediaIcons />
+      <div className={css.infoWrap}>
+        <div>
+          <Logo />
+          <p className={css.text}>
+            Get the medicine to help you feel better, get back to your active
+            life, and enjoy every moment.
+          </p>
+        </div>
+
+        <div className={css.navigationAndLinksWrap}>
+          <ul className={css.navigationList}>
+            {navLinks.map(({ to, label }) => (
+              <li className={css.listItem} key={to}>
+                <NavLink
+                  to={to}
+                  className={({ isActive }) =>
+                    isActive ? `${css.link} ${css.activeLink}` : css.link
+                  }
+                >
+                  {label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+
+          <div className={css.linksWrap}>
+            <SocialMediaIcons />
+          </div>
+        </div>
+      </div>
       <hr className={css.horizontLine} />
       <div className={css.footerBottomWrap}>
         <FooterBottom />
-        <FooterLinks />
       </div>
     </footer>
   );
--- src/components/SharedLoyaut/Footer/FooterBottom/FooterBottom.jsx
+++ src/components/SharedLoyaut/Footer/FooterBottom/FooterBottom.jsx
@@ -2,8 +2,30 @@ import css from "./FooterBottom.module.css";
 
 export default function FooterBottom() {
   return (
-    <p className={css.text}>
-      © E-Pharmacy {new Date().getFullYear()}. All Rights Reserved
-    </p>
+    <ul className={css.footerLinks}>
+      <li className={css.text}>
+        <p className={css.text}>
+          © E-Pharmacy {new Date().getFullYear()}. All Rights Reserved
+        </p>
+      </li>
+      <li className={css.text}>
+        <a
+          href="https://www.termsfeed.com/live/c7932450-cd4e-45ec-8f32-276829380df7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Privacy Policy
+        </a>
+      </li>
+      <li className={css.text}>
+        <a
+          href="https://www.termsfeed.com/live/18e3508a-4358-4a31-a4f6-96b3d1e203cc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Terms & Conditions
+        </a>
+      </li>
+    </ul>
   );
 }
