805e56353d65dcd617795259b01ddca45d11c92d
Serhii Tyron <mestn1y@hotmail.com>
2025-04-13 11:00:00 +0300

    add styles for login and register page on desktop screen

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -5,25 +5,27 @@ export default function RegisterPage() {
   return (
     <section className={css.registerPage}>
       <Logo classNameText={css.logoText} />
-      <div className={css.titleWrap}>
-        <h1 className={css.title}>
-          Your medication, delivered Say goodbye to all{" "}
-          <span>your healthcare</span> worries with us
-        </h1>
-        <picture>
-          <source
-            media="(min-width: 768px)"
-            srcSet="/images/tabl-desc.png 1x, /images/tabl-desc-x2.png 2x"
-          />
-          <img
-            src="/images/tabl-mob.png"
-            srcSet="/images/tabl-mob.png 1x, /images/tabl-mob-x2.png 2x"
-            alt="Pharmacy promotion"
-            className={css.image}
-          />
-        </picture>
+      <div className={css.contentWrap}>
+        <div className={css.titleWrap}>
+          <h1 className={css.title}>
+            Your medication, delivered Say goodbye to all{" "}
+            <span>your healthcare</span> worries with us
+          </h1>
+          <picture>
+            <source
+              media="(min-width: 768px)"
+              srcSet="/images/tabl-desc.png 1x, /images/tabl-desc-x2.png 2x"
+            />
+            <img
+              src="/images/tabl-mob.png"
+              srcSet="/images/tabl-mob.png 1x, /images/tabl-mob-x2.png 2x"
+              alt="Pharmacy promotion"
+              className={css.image}
+            />
+          </picture>
+        </div>
+        <RegisterForm />
       </div>
-      <RegisterForm />
       <div className={css.decoration}>
         <span className={css.decorationElement}></span>
         <span className={css.decorationElement}></span>
