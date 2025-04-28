218cfc245ae046508356bfae8c5742ceb536595e
Serhii Tyron <mestn1y@hotmail.com>
2025-04-29 00:22:01 +0300

    change private route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,26 +1,9 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-import { useEffect, useRef } from "react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
-  const navigate = useNavigate();
-  const location = useLocation();
-  const hasShownToast = useRef(false);
 
-  useEffect(() => {
-    if (shouldRedirect) {
-      if (!hasShownToast.current) {
-        toast.info("Please log in or register to access the Medicine page!");
-        hasShownToast.current = true;
-      }
-      navigate(redirectTo, { state: { from: location } });
-      hasShownToast.current = false;
-    }
-  }, [shouldRedirect, redirectTo, navigate, location]);
-
-  return !shouldRedirect ? <Component /> : null;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
